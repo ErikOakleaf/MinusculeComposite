2dfaef5e5fa12989b5f0aeb456427176abc6c7f4
Erik Ekblad <oakl33f123@gmail.com>
2026-03-29 18:11:38 +0200

    kept working on lexer and added tests

--- src/tokenizer.zig
+++ src/tokenizer.zig
@@ -1,11 +1,17 @@
 const std = @import("std");
 
 const Token = union(enum) {
-    Stirng: []const u8,
+    String: []const u8,
     Identifier: []const u8,
     LParen,
     RParen,
     Equals,
+    EOF,
+};
+
+const LexError = error{
+    IdentifierStartsWithNumber,
+    UnterminatedString,
 };
 
 const Lexer = struct {
@@ -15,24 +21,26 @@ const Lexer = struct {
     ch: u8, // current char under examination
 
     fn init(input: []const u8) Lexer {
-        var lexer = Lexer{ .input = input };
+        var lexer = Lexer{
+            .input = input,
+            .pos = 0,
+            .read_pos = 0,
+            .ch = undefined,
+        };
         lexer.read_ch();
         return lexer;
     }
 
-    fn next_token(self: *Lexer) Token {
+    fn next_token(self: *Lexer) !Token {
+        self.skip_whitespace();
+
         const token = switch (self.ch) {
             '(' => Token.LParen,
             ')' => Token.RParen,
             '=' => Token.Equals,
-            '"' => {
-                const str = self.read_str();
-                return Token.String(str);
-            },
-            else => {
-                const identifier = read_identifier(self);
-                return Token.Identifier(identifier);
-            }
+            0 => Token.EOF,
+            '"' => Token{ .String = try self.read_str() },
+            else => Token{ .Identifier = try self.read_identifier() },
         };
 
         self.read_ch();
@@ -51,12 +59,16 @@ const Lexer = struct {
     }
 
     fn skip_whitespace(self: *Lexer) void {
-        while (self.ch == ' ') {
+        while (self.ch == ' ' or self.ch == '\n' or self.ch == '\t' or self.ch == '\r') {
             self.read_ch();
         }
     }
 
-    fn read_identifier(self: *Lexer) []const u8 {
+    fn read_identifier(self: *Lexer) ![]const u8 {
+        if (self.ch >= '0' and self.ch <= '9') {
+            return error.IdentifierStartsWithNumber;
+        }
+
         const start_pos = self.pos;
 
         while (is_letter(self.ch)) {
@@ -66,7 +78,7 @@ const Lexer = struct {
         return self.input[start_pos..self.pos];
     }
 
-    fn read_str(self: *Lexer) []const u8 {
+    fn read_str(self: *Lexer) ![]const u8 {
         self.read_ch(); // skip the first ' " ' so it does not get caught in the loop
 
         const start_pos = self.pos;
@@ -76,7 +88,7 @@ const Lexer = struct {
         }
 
         if (self.ch == 0) {
-            @panic("unterminated string literal");
+            return error.UnterminatedString;
         }
 
         const result = self.input[start_pos..self.pos];
@@ -90,4 +102,60 @@ fn is_letter(ch: u8) bool {
         '0'...'9', 'A'...'Z', 'a'...'z', '_' => true,
         else => false,
     };
+}
+
+test "next_token returns identifier for letter and digit sequences" {
+    const input = "tester123";
+    var lexer = Lexer.init(input);
+    const token = try lexer.next_token();
+    switch (token) {
+        .Identifier => |val| try std.testing.expectEqualStrings("tester123", val),
+        else => return error.TestUnexpectedResult,
+    }
+}
+
+test "next_token returns string for sequence within \" \"" {
+    const input = "\"hello world\"";
+    var lexer = Lexer.init(input);
+    const token = try lexer.next_token();
+    switch (token) {
+        .String => |val| try std.testing.expectEqualStrings("hello world", val),
+        else => return error.TestUnexpectedResult,
+    }
+}
+
+test "next_token returns Equals for '='" {
+    const input = "=";
+    var lexer = Lexer.init(input);
+    const token = try lexer.next_token();
+    try std.testing.expect(token == Token.Equals);
+}
+
+test "next_token returns LParen for '('" {
+    const input = "(";
+    var lexer = Lexer.init(input);
+    const token = try lexer.next_token();
+    try std.testing.expect(token == Token.LParen);
+}
+
+test "next_token returns RParen for ')'" {
+    const input = ")";
+    var lexer = Lexer.init(input);
+    const token = try lexer.next_token();
+    try std.testing.expect(token == Token.RParen);
+}
+
+test "next_token returns EOF at the end of the input" {
+    const input = ")";
+    var lexer = Lexer.init(input);
+    var token = try lexer.next_token();
+    token = try lexer.next_token();
+    try std.testing.expect(token == Token.EOF);
+}
+
+test "next_token errors on identifier with leading number as input" {
+    const input = "123something";
+    var lexer = Lexer.init(input);
+    const err = lexer.next_token();
+    try std.testing.expectError(error.IdentifierStartsWithNumber, err);
 }
